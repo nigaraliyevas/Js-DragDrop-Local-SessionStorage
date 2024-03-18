8991c4a5e22a87f9856239b5c52a7076a53ec947
nigaraliyevas <naliyeva965@gmail.com>
2024-03-18 14:41:08 +0400

    init

---
+++ assets/js/areas.js
@@ -0,0 +1,28 @@
+const boxes = document.querySelectorAll(".box");
+const areas = document.querySelectorAll(".area");
+
+boxes.forEach(box => {
+  box.addEventListener("dragstart", function(ev) {
+    const dataId = this.getAttribute("data-id");
+    ev.dataTransfer.setData("data-ID", dataId); 
+  });
+});
+areas.forEach(area => {
+  area.addEventListener("dragover", function(ev) {
+    ev.preventDefault();
+  });
+
+  area.addEventListener("drop", function(ev) {
+    ev.preventDefault();
+    let id = ev.dataTransfer.getData("data-ID");
+    const parseId = parseInt(id);
+    const start=parseInt(this.id.split("-")[0]);
+    const end=parseInt(this.id.split("-")[1]);
+    if (parseId >= start && parseId <= end) {
+      this.append(document.querySelector(`[data-id="${id}"]`));
+    }
+    else{
+      alert("Not right area!")
+    }
+  });
+});
